04acf6cac0c43e4e027f9530758c6ac38de92ae0
joh-mueller <102861062+joh-mueller@users.noreply.github.com>
2025-04-16 14:16:53 +0200

    Fixing highlighting quiz.js part 2

--- private/quiz/js/quiz.js
+++ private/quiz/js/quiz.js
@@ -354,28 +354,6 @@
       if (questionNumber === myQuestions.length - 1) {
         showResults();
       }
-
-      // Zoom to correct map location if mapId exists
-      if (currentQuestion.mapId !== undefined && window.map) {
-        fetch("../data/spots.geojson")
-          .then((response) => response.json())
-          .then((data) => {
-            const feature = data.features.find(f => f.properties.id === currentQuestion.mapId);
-            if (feature) {
-              const coords = feature.geometry.coordinates;
-              map.flyTo({
-                center: coords,
-                zoom: 6,
-                speed: 1.2,
-                essential: true
-              });
-            } else {
-              console.warn("Feature not found for mapId:", currentQuestion.mapId);
-            }
-
-          })
-          .catch(err => console.error("GeoJSON fetch error:", err));
-      }
     }
   });
   
